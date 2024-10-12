1b19b93754b21d3ae1efa6016e2f7dce199e4e65
Brian <brianizzo93@gmail.com>
2024-10-12 18:00:51 +0900

    added express async errors to use errorhandler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,28 @@
-import express from "express";
+const express = require('express')
+
+
+const app = express();
+require('express-async-errors')
+const cors = require('cors');
+
 import todoRoutes from "./routes/todoRoutes";
-import userRoutes from "./routes/userRoutes"
+import userRoutes from "./routes/userRoutes";
+const middleware = require('./utils/middleware');
 
+app.use(cors());
+app.use(express.static('dist'))
+app.use(express.json());
 
-const cors = require('cors')
-const middleware = require('./utils/middleware')
+app.use(middleware.requestLogger);
 
-
-const app = express()
-app.use(cors())
-app.use(express.json())
 app.use("/", todoRoutes);
-app.use("/", userRoutes)
-app.use(middleware.requestLogger)
+app.use("/", userRoutes);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+
 
 
 module.exports = app
